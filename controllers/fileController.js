1e70603c9e3e0fc5066d9e3efe005cdc12c95e03
Fuad Hamdi Bahar <fuadhamdi99@gmail.com>
2023-10-16 16:09:46 +0800

    kalau sudah unduh update status jadi 1

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,45 +1,90 @@
+const {
+  SpFromDesa,
+  SuketWali,
+  SuketBlmKtp,
+  SuketBlmPunyaRumah,
+  SpToDesa,
+  SuperImunisasi,
+  SuperUtangPiutang,
+  SuketJabatan,
+  SuketDomisili,
+  SuketGhoib,
+  SuketKehilanganStpd,
+  SuketKehilanganKK,
+  SuketKekerabatan,
+  SuketLahirDesa,
+  SuketLokasiTanah,
+  SuketMenantu,
+  SuketPendudukLiar,
+  SuketPenegasanWil,
+  SuketAktifPerusahaan,
+  SuketUntukMenikah,
+  SuketTelahMenikah,
+  SuketPisahRumah,
+  SuketPernyataanWaris,
+  SuketPengurusanPBB,
+  SuketPengurusanKK,
+  Imbs,
+} = require("../models");
+
 const path = require('path');
 const fs = require('fs');
 
 exports.downloadFile = async (req, res) => {
-    try {
-        const file = req.query.filename;
-        const pathFile = path.join(
-          __dirname,
-          "..",
-          "public",
-          "dokumen",
-          `${file}`
-        );
-        const fileType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+  try {
+    const file = req.query.filename;
+    const form = req.query.form;
 
-        if (fs.existsSync(pathFile)) {
-          res.setHeader(
-            "Content-Disposition",
-            `attachment; filename=${file}.docx`
-          );
-          res.setHeader("Content-Type", fileType);
+    const pathFile = path.join(
+      __dirname,
+      "..",
+      "public",
+      "dokumen",
+      `${file}`
+    );
+    const fileType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
 
-          // Stream the file to the response
-          const fileStream = fs.createReadStream(pathFile);
-          fileStream.pipe(res);
-        } else {
-          return res.status(404).json({
-            status: false,
-            msg: "File not found",
-          });
-        }
-        
-    } catch (error) {
-        console.error(
-          "Error while fetching SuketPengurusanKK data:",
-          error.message
+    if (fs.existsSync(pathFile)) {
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=${file}.docx`
+      );
+      res.setHeader("Content-Type", fileType);
+
+      // update bahwa file telah di download
+      if (form == "imbs") {
+        await Imbs.update(
+          {
+            status: 1,
+          },
+          {
+            where: {
+              fileName: file,
+            },
+          }
         );
-        return res.status(500).json({
-          status: false,
-          msg: "Internal Server Error",
-        });
+      }
+
+      // Stream the file to the response
+      const fileStream = fs.createReadStream(pathFile);
+      fileStream.pipe(res);
+    } else {
+      return res.status(404).json({
+        status: false,
+        msg: "File not found",
+      });
     }
+
+  } catch (error) {
+    console.error(
+      "Error while fetching SuketPengurusanKK data:",
+      error.message
+    );
+    return res.status(500).json({
+      status: false,
+      msg: "Internal Server Error",
+    });
+  }
 }
 
 
