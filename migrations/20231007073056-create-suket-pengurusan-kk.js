059e1344e95ce6628a98b7ff04ef388dfa57a39e
Fuad Hamdi Bahar <fuadhamdi99@gmail.com>
2023-10-20 13:34:09 +0800

    fix some migrations

--- migrations/20231007073056-create-suket-pengurusan-kk.js
+++ migrations/20231007073056-create-suket-pengurusan-kk.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("suketpengurusankk", {
+    await queryInterface.createTable("suketuruskk", {
       id: {
         allowNull: false,
         autoIncrement: true,
